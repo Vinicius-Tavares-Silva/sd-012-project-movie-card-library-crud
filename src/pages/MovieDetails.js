2a1cfd7dfa4c5a17bfba3c4ef5004518a17eb82d
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-07-07 15:59:16 -0300

    Requisito 3 - Renderiza os MovieCards

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movieDetails: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  async fetchAPI() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      movieDetails: { ...movie },
+      loading: false,
+    });
+  }
+
+  render() {
+    const {
+      title,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      subtitle } = this.state.movieDetails;
+    const { loading } = this.state;
+    if (loading) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-details">
