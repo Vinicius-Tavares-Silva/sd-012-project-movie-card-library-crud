679df930760956e83261a4577dcb3dc8cb0a6e7b
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-07 19:30:29 -0300

    R3- insere link em MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, imagePath } = movie;
+    const { id, title, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
         <img src={ imagePath } alt={ title } />
         <p>{ title }</p>
         <p>{ storyline }</p>
-        Movie Card
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
