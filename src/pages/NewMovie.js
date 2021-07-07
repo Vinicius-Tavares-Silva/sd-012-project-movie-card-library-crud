784be39f2bc363a17cae588b09c01bbc238e0ea5
Marcela Silva <marcela.ops@hotmail.br>
2021-07-07 16:44:21 -0300

    req6 Link no MovieList e NewMovie com form e requisição

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-
-// import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router';
+import MovieForm from '../components/MovieForm';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      redirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(/* newMovie */) {
+  // Requisito 6
+  async handleSubmit(newMovie) {
+    await movieAPI.createMovie(newMovie);
+    this.setState({
+      redirect: true,
+    });
   }
 
   render() {
+    const { redirect } = this.state;
+    if (redirect) return <Redirect to="/" />;
+
     return (
       <div data-testid="new-movie">
-        {/* <MovieForm onSubmit={ this.handleSubmit } /> */}
+        <MovieForm onSubmit={ this.handleSubmit } />
       </div>
     );
   }
