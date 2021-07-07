57c09f512428ac5d36d97a966d9fbd781cd779b6
IsaacCruzLM <isaac.clm1@gmail.com>
2021-07-07 14:50:24 -0300

    Requisito 1 - Adiciona rotas no App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import * as routes from './pages/index';
 
 function App() {
+  const { MovieList, NewMovie, EditMovie, MovieDetails, NotFound } = routes;
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Header</div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route
+          path="/movies/:id/edit"
+          render={ (props) => <EditMovie { ...props } /> }
+        />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route exact path="*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
