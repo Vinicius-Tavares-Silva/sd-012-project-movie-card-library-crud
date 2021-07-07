f939049f387965fd819769c2d30a428303667cea
pedro21medeiros <pedro21medeiros@gmail.com>
2021-07-07 15:06:26 -0300

    altera rota do MovieDetails para render

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function App() {
       <Switch>
         <Route path="/" exact component={ MovieList } />
         <Route path="/movies/new" exact component={ NewMovie } />
-        <Route path="/movies/:id" exact component={ MovieDetails } />
+        {/* <Route path="/movies/:id" exact component={ MovieDetails } /> */}
+        <Route
+          path="/movies/:id"
+          exact
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
         <Route path="/movies/:id/edit" exact component={ EditMovie } />
         <Route component={ NotFound } />
       </Switch>
