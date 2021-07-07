5fbba920be8c8ba61cb687bd1a218a2eef70235c
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-07 18:29:20 -0300

    R2-requisicao com moviecard e mensagem de Loading...

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
+        <img src={ imagePath } alt={ title } />
+        <p>{ title }</p>
+        <p>{ storyline }</p>
         Movie Card
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
