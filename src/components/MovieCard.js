d68fc654bab404b5b0a61b371314727d3ca67ca4
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-07-07 17:36:59 -0300

    insert a link for page of details of a movie in MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, imagePath, id } = movie;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <h3>{title}</h3>
+        <img src={ imagePath } alt={ title } />
+        <div>{storyline}</div>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
