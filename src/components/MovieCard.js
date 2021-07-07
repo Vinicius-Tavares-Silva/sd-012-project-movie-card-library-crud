3462d91a91b50589d6fdd86acdb1a9cbe8732b59
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-07-07 12:51:31 -0300

    Requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,36 @@
+// import { tokTypes } from 'acorn';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, id, rating } = movie;
     return (
       <div data-testid="movie-card">
+        <img src={ imagePath } alt={ title } />
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <p>{storyline}</p>
+        <p>
+          Rating:
+          {rating}
+        </p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
         Movie Card
       </div>
     );
   }
 }
-
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    id: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
 export default MovieCard;
