6d156a6900e5ee7b652c7a6dfd5bf3a4d9c53bb6
Tiago Ornelas <tiagoornelas@pop-os.localdomain>
2021-07-07 18:08:13 -0300

    Resolve o requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,21 +2,47 @@ import React, { Component } from 'react';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: [],
+      response: false,
+    };
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    movieAPI
+      .getMovie(id)
+      .then((data) => this.setState({ movie: data }))
+      .then(() => this.setState({ response: true }));
+  }
+
+  render() {
+    const { movie, response } = this.state;
+
+    // Render Loading here if the request is still happening
+    if (!response) {
+      return <Loading />;
+    }
+
+    const { id } = this.props.match.params;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        <img alt="Movie Cover" src={`../${imagePath}`} />
+        <h2>{title}</h2>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
       </div>
     );
   }
