d5dc80b2fe7494e8bb5ffce6d093cb48485e0e5a
RenanMiyashita <renanmiyashita@gmail.com>
2021-07-07 10:35:46 -0300

    resolve req 5 (EditMovie)

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,34 +1,68 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+      load: true,
+      submitted: false,
+    };
+    this.getMovieData = this.getMovieData.bind(this);
+    this.renderMovieEdit = this.renderMovieEdit.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  componentDidMount() {
+    this.getMovieData();
   }
 
-  render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
+  handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ submitted: true });
+  }
 
-    if (status === 'loading') {
-      // render Loading
-    }
+  async getMovieData() {
+    const { match: { params: { id } } } = this.props;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      movie,
+      load: false,
+    });
+  }
 
+  renderMovieEdit() {
+    const { movie } = this.state;
     return (
       <div data-testid="edit-movie">
         <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
+
+  render() {
+    const { load, submitted } = this.state;
+    const loadingComponent = <Loading />;
+    const movieEditComponent = this.renderMovieEdit();
+    const editPage = load ? loadingComponent : movieEditComponent;
+    const homePage = <Redirect to="/" />;
+
+    return (
+      <div>
+        { submitted ? homePage : editPage }
+      </div>
+    );
+  }
 }
 
 export default EditMovie;
+
+EditMovie.propTypes = {
+  match: PropTypes.objectOf(
+    PropTypes.object,
+  ).isRequired,
+};
