238cf11ecbc7125176ea46b3b73b52f3e0a15cbd
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-07-07 13:15:26 -0300

    Requisito 2 - Renderiza MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,13 +9,30 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+
+    this.fetchAPI = this.fetchAPI.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  async fetchAPI() {
+    const movies = await movieAPI.getMovies();
+    this.setState({
+      movies: [...movies],
+      loading: false,
+    });
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
-    // Render Loading here if the request is still happening
+    if (loading) {
+      return 'Carregando...';
+    }
 
     return (
       <div data-testid="movie-list">
