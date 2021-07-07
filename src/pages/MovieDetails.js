6bcb0472bfc2e6e564fcb5892b98116433be0041
Rodrigo <rodrigo.facury14@gmail.com>
2021-07-07 17:32:44 -0300

    Tentativa Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+    };
+
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    movieAPI.getMovie(id)
+      .then((correctMovie) => this.setState({ movie: correctMovie }));
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-      </div>
+      Object.entries(movie).length === 0 ? <Loading />
+        : (
+          <div data-testid="movie-details">
+            <img alt="Movie Cover" src={ `../${imagePath}` } />
+            <p>{ title }</p>
+            <p>{ `Subtitle: ${subtitle}` }</p>
+            <p>{ `Storyline: ${storyline}` }</p>
+            <p>{ `Genre: ${genre}` }</p>
+            <p>{ `Rating: ${rating}` }</p>
+            <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+            <Link to="/">VOLTAR</Link>
+          </div>)
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
