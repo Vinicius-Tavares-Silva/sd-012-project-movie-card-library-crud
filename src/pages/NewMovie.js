492c65217f63aa81da1c8b67d842dc5545d41d37
lucasnahas <lucasahas@gmail.com>
2021-07-07 11:23:53 -0300

    faz add movie

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
-
-import MovieForm from '../components/MovieForm';
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
-class NewMovie extends Component {
+export default class NewMovie extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loading: false,
+      redirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    this.setState({
+      loading: true,
+    });
+    movieAPI.createMovie(newMovie)
+      .then(() => {
+        this.setState({
+          redirect: true,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          loading: 'Erro ao carregar',
+        });
+      });
   }
 
   render() {
+    const { state: { loading, redirect } } = this;
+    if (redirect) return <Redirect to="/" />;
+    if (loading) return <Loading />;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
@@ -20,4 +42,3 @@ class NewMovie extends Component {
     );
   }
 }
-export default NewMovie;
