9078010d47a39b40c2f35f75ae8a242d85fa9b93
AlyssonSene <alysson_silva@outlook.com>
2021-07-07 17:48:27 -0300

    requisitos 1 e 2 implementados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound} from './pages';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/movies/:id/edit" component={ EditMovie } />
+            <Route path="/movies/new" component={ NewMovie } />
+            <Route path="/movies/:id" component={ MovieDetails } />
+            <Route exact path="/" component={ MovieList } />
+            <Route exact path="/*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
