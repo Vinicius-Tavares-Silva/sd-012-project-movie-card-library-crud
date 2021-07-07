5f4e8a4bf2166ea203576b85ce5074c5794cad3d
CamilaDamasio <camiladamasioef@hotmail.com>
2021-07-07 14:40:06 -0300

    Requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -12,14 +12,17 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then((result) => this.setState({ movies: result }));
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { !movies.length ? <Loading /> : console.log('')}
       </div>
     );
   }
