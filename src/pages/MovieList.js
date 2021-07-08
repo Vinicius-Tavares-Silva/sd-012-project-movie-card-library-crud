2df1524f9589e3d92a469de5f0d1e9fdc8bf42cc
Guilherme Dias <gmd.delta@gmail.com>
2021-07-08 01:03:44 -0300

    [PT-BR] Resolvido requisito 2 e 3. [EN] Solved 2nd and 3rd requirement

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import { getMovies } from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,19 +9,30 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      getInfo: 0,
     };
   }
 
-  render() {
+  componentDidMount() {
+    getMovies().then((movieArr) => this.setState({ movies: [...movieArr], getInfo: 1 }));
+  }
+
+  makeMovieCards = () => {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
+  }
+
+  callLoading = () => <Loading />
+
+  render() {
+    const { getInfo } = this.state;
+    const { makeMovieCards, callLoading } = this;
+
+    return (getInfo === 0) ? callLoading() : makeMovieCards();
   }
 }
 
