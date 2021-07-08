79e406202910d749e299358d1c18099b0072898d
rachidb5 <jordan.rachid@gmail.com>
2021-07-07 22:41:51 -0300

    Todos os requisitos obrigatorios completos

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class EditMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+      loading: true,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
   handleSubmit(updatedMovie) {
+    const { updateMovie } = movieAPI;
+    this.setState({ shouldRedirect: false }, async () => {
+      const movie = await updateMovie(updatedMovie);
+      this.setState({
+        shouldRedirect: true,
+        movie,
+      });
+    });
+  }
+
+  fetchMovie() {
+    const { match: { params: { id } } } = this.props;
+    const { getMovie } = movieAPI;
+    this.setState({ loading: true }, async () => {
+      const movie = await getMovie(id);
+      this.setState({
+        loading: false,
+        movie,
+      });
+    });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
+    const { shouldRedirect, loading, movie } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
 
-    if (status === 'loading') {
-      // render Loading
-    }
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="edit-movie">
@@ -30,5 +56,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      movie: {},
+    };
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie() {
+    const { match: { params: { id } } } = this.props;
+    const { getMovie } = movieAPI;
+    this.setState({ loading: true }, async () => {
+      const movie = await getMovie(id);
+      this.setState({
+        loading: false,
+        movie,
+      });
+      console.log(movie);
+    });
+  }
+
   render() {
     // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { movie, loading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
+    console.log(movie);
 
-    const { /* title, */ storyline, imagePath, genre, rating, subtitle } = {};
-
+    if (loading) return <Loading />;
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+      <div className="card" data-testid="movie-details">
+        <img className="card-img-top" alt="Movie Cover" src={ `../${imagePath}` } />
+        <div className="card-body">
+          <h4 className="card-title">{ `Subtitle: ${title}` }</h4>
+          <p className="card-text">{ `Subtitle: ${subtitle}` }</p>
+          <p className="card-text">{ `Storyline: ${storyline}` }</p>
+          <p className="card-text">{ `Genre: ${genre}` }</p>
+          <p className="card-text">{ `Rating: ${rating}` }</p>
+          <Link to="/" className="btn btn-primary">VOLTAR</Link>
+          <Link to={ `/movies/${id}/edit` } className="btn btn-primary">EDITAR</Link>
+        </div>
+
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
