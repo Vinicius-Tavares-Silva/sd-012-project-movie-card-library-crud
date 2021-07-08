4fb4004bab1941735d50888d778545cc60d1b25f
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-08 10:51:50 -0300

    R4- requisicao de filme renderizado MovieDetails,EDITAR,VOLTAR,DELETAR

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import PropTypes from 'prop-types';
 
 class MovieDetails extends Component {
   constructor() {
@@ -13,20 +13,21 @@ class MovieDetails extends Component {
       loading: true,
     };
   }
-componentDidMount() {
-  const { match: { params: { id } } } = this.props;
-  movieAPI.getMovie(id).then((response) => this.setState({
-    movie: response,
-    loading: false,
-  }));
-}
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    movieAPI.getMovie(id).then((response) => this.setState({
+      movie: response,
+      loading: false,
+    }));
+  }
 
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { 
-      movie: { id, title,storyline, imagePath, genre, rating, subtitle },
+    const {
+      movie: { id, title, storyline, imagePath, genre, rating, subtitle },
       loading,
     } = this.state;
 
@@ -37,11 +38,11 @@ componentDidMount() {
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Title: ${title}` }</p>
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        <p>{`Title: ${title}`}</p>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
         <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
         <Link to="/">VOLTAR</Link>
         <Link to="/" onClick={ () => movieAPI.deleteMovie(id) }>DELETE</Link>
@@ -52,10 +53,10 @@ componentDidMount() {
 
 MovieDetails.propTypes = {
   match: PropTypes.shape({
-  params: PropTypes.shape({
-  id: PropTypes.string.isRequired,
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
   }).isRequired,
- }).isRequired,
 };
 
 export default MovieDetails;
