c5d1301825e6000dc69c5b9f7c9dbc2a7b56a019
Sarah <sarahgsb@hotmail.com>
2021-07-08 17:12:21 -0300

    coloca imagem dos filmes na pagina

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, id } = movie;
+    const { title, imagePath, storyline, id } = movie;
 
     return (
       <div data-testid="movie-card">
@@ -15,6 +15,7 @@ class MovieCard extends React.Component {
         <p>
           { storyline }
         </p>
+        <img src={ imagePath } alt={ title } />
         <Link to={ `/movies/${id}` }> VER DETALHES </Link>
       </div>
     );
@@ -24,6 +25,7 @@ class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
+    imagePath: PropTypes.string,
     storyline: PropTypes.string,
     id: PropTypes.number,
   }).isRequired,
