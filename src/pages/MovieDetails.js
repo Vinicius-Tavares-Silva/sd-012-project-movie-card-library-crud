f8f2ec0c1069736afb3d5bd094eb01d6f882220c
Guilherme Dias <gmd.delta@gmail.com>
2021-07-08 01:30:58 -0300

    [PT-BR] Resolvido requisito 4. [EN] Solved 4th requirement

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
-
-import * as movieAPI from '../services/movieAPI';
+import { Link } from 'react-router-dom';
+import { getMovie } from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: {},
+      getInfo: 0,
+    };
+  }
 
+  componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    getMovie(id).then((mObj) => this.setState({ movie: { ...mObj }, getInfo: 1 }));
+  }
+
+  showMovieDatails = () => {
+    const { movie } = this.state;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h2>{ `Title: ${title}` }</h2>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
+  }
+
+  callLoading = () => <Loading />
+
+  render() {
+    const { getInfo } = this.state;
+    const { callLoading, showMovieDatails } = this;
+    return (getInfo === 0) ? callLoading() : showMovieDatails();
   }
 }
 
