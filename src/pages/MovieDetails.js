2fd566216006d4c8d2854650532e42269ca8bc45
Rodrigo <rodrigo.facury14@gmail.com>
2021-07-07 21:10:29 -0300

    Renderiza corretamente o DeleteLink

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
-import { Loading, DeleteLink } from '../components';
+import { Loading } from '../components';
+import DeleteLink from '../components/DeleteLink';
 
 class MovieDetails extends Component {
   constructor() {
