9fd072cc0f38a1ca829f10d35953b88c3996b784
Guilherme Dias <gmd.delta@gmail.com>
2021-07-08 02:44:05 -0300

    [PT-BR] Resolvido requisito 6. [EN] Solved 6th requirement

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
-import * as movieAPI from '../services/movieAPI';
+import { createMovie } from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      getInfo: 0,
+    };
   }
 
-  handleSubmit(newMovie) {
+  handleSubmit = (newMovie) => {
+    createMovie(newMovie).then(this.setState({ getInfo: 1 }));
   }
+
+  showNewMovie = () => (
+    <div data-testid="new-movie">
+      <MovieForm onSubmit={ this.handleSubmit } />
+    </div>
+  )
+
+  goMovieList = () => <Redirect to="/" />
 
   render() {
-    return (
-      <div data-testid="new-movie">
-        <MovieForm onSubmit={ this.handleSubmit } />
-      </div>
-    );
+    const { getInfo } = this.state;
+    const { showNewMovie, goMovieList } = this;
+    return (getInfo === 0) ? showNewMovie() : goMovieList();
   }
 }
 export default NewMovie;
