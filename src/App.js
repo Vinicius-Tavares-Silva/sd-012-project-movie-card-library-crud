e1dafffee94e7e5c3a863c21b5893f4c439eac00
Guilherme Dias <gmd.delta@gmail.com>
2021-07-07 23:29:03 -0300

    [PT-BR] Resolvido requisito 1. [EN] Solved 1st requirement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/" component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
