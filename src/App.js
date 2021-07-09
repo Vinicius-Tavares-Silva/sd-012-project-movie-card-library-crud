2ef63c7800a7f2789ea6afbca40426cf41f5c206
Eric Faria <ericdeofaria@gmail.com>
2021-07-09 13:05:16 -0300

    Requisito 02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={ MovieList }
+          />
+          <Route
+            path="/movies/:id"
+            exact
+            render={ (props) => <MovieDetails { ...props } /> }
+          />
+          <Route
+            path="/movies/new"
+            exact
+            component={ NewMovie }
+          />
+          <Route
+            path="/movies/:id/edit"
+            exact
+            render={ (props) => <EditMovie { ...props } /> }
+          />
+          <Route
+            component={ NotFound }
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
