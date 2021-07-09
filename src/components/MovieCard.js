0f060d57f5249803983429ec68c9e9dc6f1ac26e
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-07-08 23:51:10 -0300

    adiciona req 2 e comeco do 3. Falta o proptypes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    // const { movie } = this.props;
+    const { movie } = this.props;
+    const { title, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
-        {/* <div>{movie.title}</div>
-        <div>{movie.storyline}</div> */}
+        <h3 data-testid="movie-card-title" className="movie-card-title">{title}</h3>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
+        Movie Card
       </div>
     );
   }
