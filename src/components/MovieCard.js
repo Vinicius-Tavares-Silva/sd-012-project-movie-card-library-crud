ff2d3687a8e7b7e63ad382d735a258471122779f
Eric Faria <ericdeofaria@gmail.com>
2021-07-09 16:00:41 -0300

    Requisito 03

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -13,7 +13,9 @@ class MovieCard extends React.Component {
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
+        <button type="button">
+          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
+        </button>
       </div>
     );
   }
