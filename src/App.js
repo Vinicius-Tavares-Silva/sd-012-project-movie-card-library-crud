95168c8f591caf3ac78e4ee9b39f8025985eca14
walttoncoelho <walltton@outlook.com>
2021-07-10 08:27:29 -0300

    App BrouserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+
+    <BrowserRouter>
+      <h1>Movie Card Library CRUD</h1>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="" component={ NotFound } />
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
