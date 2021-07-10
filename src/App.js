012e99d9f8c199f4eb691c5d2f2226da9280ba7e
LuanAlexandre <luanrb19@gmail.com>
2021-07-10 13:50:52 -0400

    Dia 13.3 - projeto-movie-card-library-crud: requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MovieList, EditMovie, MovieDetails, NewMovie, NotFound } from './pages';
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route exact path="/" component={ MovieList } />
+          <Route path="" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
