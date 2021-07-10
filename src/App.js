fe0a058db138ac490cd4d07abd93187b86690622
walttoncoelho <walltton@outlook.com>
2021-07-10 16:55:24 -0300

    MovieList, ComponenteDidMont

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
 
     <BrowserRouter>
-      <h1>Movie Card Library CRUD</h1>
+      <div>
+        <h1>Movie Card Library CRUD</h1>
+      </div>
       <Switch>
         <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route exact path="/movies/:id/edit" component={ EditMovie } />
-        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route exact path="" component={ NotFound } />
-        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+
       </Switch>
     </BrowserRouter>
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
   }
 
+  componentDidMount() {
+    this.movieCard();
+  }
+
+  movieCard = async () => {
+    const moviesCards = await movieAPI.getMovies();
+    this.setState({
+      loading: false, movies: [...moviesCards] });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
+        { loading && <p>Carregando...</p> }
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
