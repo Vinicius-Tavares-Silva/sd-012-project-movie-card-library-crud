0890f1b24dc2669894f8c0de754eba80455552c2
Gabriel Viana <g.viana87@hotmail.com>
2021-07-10 12:29:42 -0300

    Req 1 - Add rotas em App.js/ renderizando ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
   render() {
     return (
-      <div>Movie Card Library CRUD</div>
+      <div>
+        Movie Card Library CRUD
+        <BrowserRouter>
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="" component={ NotFound } />
+        </BrowserRouter>
+
+      </div>
     );
   }
 }
