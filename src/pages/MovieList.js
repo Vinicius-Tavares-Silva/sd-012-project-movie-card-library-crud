b0223f5f5866b511e5222ff751487385fbdec319
David Azevedo <zevid.dev@outlook.com>
2021-07-10 09:01:44 -0300

    Create renderMovieList, state loading and fetchApi in MovieList. Render loadingElement or MovieCard in MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import movies from '../services/movieData';
 
 class MovieList extends Component {
   constructor() {
@@ -9,16 +10,43 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
+  async fetchApi() {
+    const { getMovies } = movieAPI;
+    this.setState({
+      loading: true,
+    }, async () => {
+      const fetchMovies = await getMovies().then((resolve) => resolve)
+      this.setState({
+        movies: [...movies,fetchMovies],
+        loading: false,
+      })
+    })
+  }
+  componentDidMount() {
+    this.fetchApi()
+  }
+
+  renderMovieList() {
     const { movies } = this.state;
-    // Render Loading here if the request is still happening
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {movies[movies.length - 1].map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
+    )
+  }
+
+  render() {
+    const { loading } = this.state;
+    // Render Loading here if the request is still happening
+    const loadingElement = <p>Carregando...</p>
+    return (
+      <React.Fragment>
+        { loading ? loadingElement : this.renderMovieList( ) }
+      </React.Fragment>
     );
   }
 }
