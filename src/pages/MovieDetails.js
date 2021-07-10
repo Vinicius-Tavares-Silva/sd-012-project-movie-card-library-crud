3c612cf5c24c930ba31d496bb689e8aef0937ab4
LuanAlexandre <luanrb19@gmail.com>
2021-07-10 18:08:23 -0400

    Dia 13.3 - projeto-movie-card-library-crud: requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
-class MovieDetails extends Component {
+export default class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: undefined,
+    };
+
+    this.requestMovie = this.requestMovie.bind(this);
+  }
+
+  async componentDidMount() {
+    this.requestMovie();
+  }
+
+  async requestMovie() {
+    const { getMovie } = movieAPI;
+    const { match: { params: { id } } } = this.props;
+    const requestedMovie = await getMovie(id);
+    this.setState({
+      movie: requestedMovie,
+    });
+  }
+
   render() {
+    const { movie } = this.state;
     // Change the condition to check the state
     // if (true) return <Loading />;
+    if (!movie) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
 }
 
-export default MovieDetails;
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
