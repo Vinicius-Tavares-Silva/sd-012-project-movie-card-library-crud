da59c388d9a209f43ae5e9b46a234278d0ba1841
Gabriel Viana <g.viana87@hotmail.com>
2021-07-10 14:45:02 -0300

    req 2 - requisiçao API/ Component Loading render ok

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
+    this.fetchMoviesAPI = this.fetchMoviesAPI.bind(this);
+    this.movieCardList = this.movieCardList.bind(this);
+
     this.state = {
+      loading: true,
       movies: [],
     };
   }
 
-  render() {
+  componentDidMount() {
+    this.fetchMoviesAPI();
+  }
+
+  async fetchMoviesAPI() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const moviesFetch = await movieAPI.getMovies();
+        this.setState({
+          loading: false,
+          movies: moviesFetch,
+        });
+      },
+    );
+  }
+
+  movieCardList() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
+    );
+  }
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <div>{ loading ? <Loading /> : this.movieCardList() }</div>
     );
   }
 }
