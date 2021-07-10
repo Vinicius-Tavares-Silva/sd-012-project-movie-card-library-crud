6ed851f793ec519e3db154775b56349feb2b057c
Victor Emmanuel <victorpassos2012@gmail.com>
2021-07-10 19:51:08 -0300

    Fazendo imagens, subtitulo, genero e rating aparece

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,11 +5,15 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, id } = movie;
+    const { title, subtitle, imagePath, storyline, genre, rating, id } = movie;
     return (
       <div data-testid="movie-card">
         <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <img src={ imagePath } alt="imagem do filme" />
         <p>{storyline}</p>
+        <h3>{ genre }</h3>
+        <h3>{ rating }</h3>
         <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
@@ -18,9 +22,13 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string,
-    storyline: PropTypes.string,
-    id: PropTypes.number,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
   }).isRequired,
 };
 
