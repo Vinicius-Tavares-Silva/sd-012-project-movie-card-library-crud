a1023ecb4b2dc64cf4c2c8856641158a732b5f46
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-07-11 11:45:55 -0300

    Inicia App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-    </Router>
+      <Switch>
+        <Route path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" render={ (componentProps) => <MovieDetails {...componentProps} /> } />
+        <Route path="/movies/:id/edit" render={ (componentProps) => <EditMovie {...componentProps} /> } />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
