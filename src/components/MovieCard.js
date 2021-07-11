51e6eaa85284685bbb0f3afa48b02ee9e37155bd
Adriana Biberg <adribiberg@hotmail.com>
2021-07-11 14:38:24 -0300

    Cumpre requisito 3 - Insere um link para a página de detalhes de um filme de MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, imagePath, subtitle, id } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h2>{ title }</h2>
+        <p>{ subtitle }</p>
+        <p>{ storyline }</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    subtitle: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
+
+// Requisito 3 cumprido com o auxílio do colega Rodrigo Facury;
 
 export default MovieCard;
