02d4ad37aaceb8b9bf2ce6cb61f76b443ceb6ed3
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-07-11 16:12:22 -0300

    Implementa MovieCard.js para requisito 3.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, subtitle, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt={ `${title} ${subtitle}` } />
+        <p>{ title }</p>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
