cee9938070095dccad17bb3d0635d791747b207e
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-07-11 17:37:40 -0300

    Fix erros do Lint até requisito 4.

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
   constructor() {
@@ -11,7 +11,7 @@ class MovieDetails extends Component {
     this.state = {
       movie: {},
       loading: true,
-    }
+    };
   }
 
   componentDidMount() {
@@ -19,7 +19,8 @@ class MovieDetails extends Component {
   }
 
   async moviesFetchApi() {
-    const { id } = this.props.match.params;
+    const { match } = this.props;
+    const { id } = match.params;
     movieAPI.getMovie(id)
       .then((response) => this.setState({
         movie: response,
@@ -28,8 +29,8 @@ class MovieDetails extends Component {
   }
 
   render() {
-    const { 
-      movie: {id, title, storyline, imagePath, genre, rating, subtitle},
+    const {
+      movie: { id, title, storyline, imagePath, genre, rating, subtitle },
       loading,
     } = this.state;
     if (loading) {
@@ -43,18 +44,26 @@ class MovieDetails extends Component {
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
-        <button>
-          <Link to='/'>VOLTAR</Link>
+        <button type="button">
+          <Link to="/">VOLTAR</Link>
         </button>
-        <button>
+        <button type="button">
           <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
         </button>
-        <button>
+        {/* <button type="button">
           <Link to=''></Link>
-        </button>
+        </button> */}
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
