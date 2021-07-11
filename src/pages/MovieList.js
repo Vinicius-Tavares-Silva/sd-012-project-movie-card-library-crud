9f6d2f3e0313ff4468cede4b0997c0da35353f63
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-07-11 15:52:12 -0300

    Implementa MovieList.js para o requisito 2.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import { Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
+    this.moviesFetchApi = this.moviesFetchApi.bind(this);
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  async moviesFetchApi() {
+    movieAPI.getMovies()
+      .then((response) => this.setState({
+        movies: response,
+        loading: false,
+      }));
+  }
+
+  componentDidMount() {
+    this.moviesFetchApi();
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
+    if (loading) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
