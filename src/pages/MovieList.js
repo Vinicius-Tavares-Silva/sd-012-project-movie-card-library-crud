df31d7f65065fb8478785de0c91248dbfbc5365d
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-07-11 19:27:48 -0300

    Commit Backup.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -33,6 +33,7 @@ class MovieList extends Component {
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
