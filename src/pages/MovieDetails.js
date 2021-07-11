0ed45abaecaaba3e774e74060d53ec78943cb85f
Adriana Biberg <adribiberg@hotmail.com>
2021-07-11 17:03:40 -0300

    Cumpre requisito 4 - Faz uma requisição para buscar o filme que sera renderizado dentro de Movie Details

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+    };
+    this.mountDetails = this.mountDetails.bind(this);
+  }
+
+  componentDidMount() {
+    this.mountDetails();
+  }
+
+  mountDetails() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    movieAPI.getMovie(id)
+      .then((idMovie) => this.setState({ movie: idMovie }));
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (Object.entries(movie).length === 0) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h2>{ `Title: ${title}` }</h2>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
