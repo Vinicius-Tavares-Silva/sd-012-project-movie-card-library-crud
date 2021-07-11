14412591cffde4cde418e64f01bc54e7920f43fd
ladcs <hpladcs@gmail.com>
2021-07-10 21:53:17 -0300

    R1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails';
-import NewMovie from './pages/NewMovie';
-import EditMovie from './pages/EditMovie';
-import NotFound from './pages/NotFound';
+import { EditMovie, MovieList, NewMovie, NotFound, MovieDetails } from './pages/index';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={ MovieList } />
-        <Route path="//movies/new" component={ NewMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/:error" component={ NotFound } />
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="/:error" component={ NotFound } />
+          <Route path="/" component={ MovieList } />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
