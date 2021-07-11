fab3bf8507df9ac3b362fdb92512114f4e4d5604
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-07-11 19:17:44 -0300

    Requisito 1 (Renderizando componentes)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Index from './pages/index';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Index />
     </Router>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,28 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import EditMovie from './EditMovie';
+import MovieDetails from './MovieDetails';
+import MovieList from './MovieList';
+import NewMovie from './NewMovie';
+import NotFound from './NotFound';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <main className="Index">
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </main>
+    );
+  }
+}
+
+export default Index;
 export { default as EditMovie } from './EditMovie';
 export { default as MovieDetails } from './MovieDetails';
 export { default as MovieList } from './MovieList';
