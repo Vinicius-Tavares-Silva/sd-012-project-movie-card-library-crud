e3d2d0d68ca8c962ce168e344e7bdd3f4054e842
Luciano Augusto <82337394+ladcs@users.noreply.github.com>
2021-07-11 11:34:14 -0300

    Update App.js
    Arrumando R1

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/movies/new" component={ NewMovie } />
-          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
           <Route path="/movies/:id" component={ MovieDetails } />
           <Route path="/:error" component={ NotFound } />
-          <Route path="/" component={ MovieList } />
+          <Route exact path="/" component={ MovieList } />
         </Switch>
       </BrowserRouter>
     </div>
