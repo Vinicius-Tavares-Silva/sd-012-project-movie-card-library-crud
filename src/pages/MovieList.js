41cdd5f0f4dacd16a0a2cb61490135115c2d1dc8
Adriana Biberg <adribiberg@hotmail.com>
2021-07-11 14:00:50 -0300

    Cumpre requisito 2 - Faz requisiçao para a API, renderiza Loading durante a requisiçao e renderiza movie card apos a montagem no dom

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import { Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -10,12 +10,25 @@ class MovieList extends Component {
     this.state = {
       movies: [],
     };
+    this.mountList = this.mountList.bind(this);
+  }
+
+  componentDidMount() {
+    this.mountList();
+  }
+
+  mountList() {
+    movieAPI.getMovies()
+      .then((movieslist) => this.setState({ movies: movieslist }));
   }
 
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+    if (movies.length === 0) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
@@ -24,5 +37,6 @@ class MovieList extends Component {
     );
   }
 }
+// Requisito 2 cumprido com o auxílio do colega Rodrigo Facury;
 
 export default MovieList;
