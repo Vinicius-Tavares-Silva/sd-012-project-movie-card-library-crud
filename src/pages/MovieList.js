78820a92b626914c9a89870cf5b14bba6d8ed54e
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-07-11 12:29:14 -0300

    Requisito 02

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -7,15 +8,40 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.fetchAPI = this.fetchAPI.bind(this);
+
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.fetchAPI();
+  }
 
-    // Render Loading here if the request is still happening
+  async fetchAPI() {
+    this.setState(
+      { loading: true },
+      async () => {
+        try {
+          const { getMovies } = movieAPI;
+          const result = await getMovies();
+          this.setState({
+            movies: [...result],
+            loading: false,
+          });
+        } catch {
+          console.log('Error: movieAPI could not fetch');
+        }
+      },
+    );
+  }
+
+  render() {
+    const { movies, loading } = this.state;
+
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="movie-list">
