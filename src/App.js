564d3c20b964188411e0c6e4296863f7cc1df58e
walttoncoelho <walltton@outlook.com>
2021-07-11 16:33:41 -0300

    corrigindo erros

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { MovieList, MovieDetails, EditMovie, NewMovie, NotFound } from './pages';
 
 function App() {
@@ -13,13 +13,13 @@ function App() {
         <Switch>
           <Route exact path="/" component={ MovieList } />
           <Route
-            path="/movies/:id"
-            render={ (props) => <MovieDetails { ...props } /> }
-          />
-          <Route
             path="/movies/new"
             render={ (props) => <NewMovie { ... props } /> }
             component={ NewMovie }
+          />
+          <Route
+            path="/movies/:id"
+            render={ (props) => <MovieDetails { ...props } /> }
           />
           <Route
             exact
@@ -29,6 +29,7 @@ function App() {
           <Route path="" component={ NotFound } />
 
         </Switch>
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </BrowserRouter>
     </div>
 
