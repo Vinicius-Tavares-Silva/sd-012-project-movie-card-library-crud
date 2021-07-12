24300f302a6e7ce7d790366d4b7bbdb1e5104956
ladcs <hpladcs@gmail.com>
2021-07-12 12:39:26 -0300

    R6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -27,14 +28,20 @@ class MovieList extends Component {
 
   render() {
     const { movies, loading } = this.state;
+    const notLoading = (
+      <div>
+        {movies
+          .map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+      </div>
+    );
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
         <h1>Lista de filmes</h1>
-        {loading ? <Loading /> : movies
-          .map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading /> : notLoading}
       </div>
     );
   }
