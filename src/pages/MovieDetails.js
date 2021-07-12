a28311ecaeef560a8779cd068e61fbb3526ca124
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-07-11 21:47:55 -0300

    exercicio 7 encerrando o projeto aqui sem uso de css

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
-import * as movieAPI from '../services/movieAPI';
+import { getMovie, deleteMovie } from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
@@ -13,7 +13,7 @@ class MovieDetails extends Component {
       load: true,
     };
     this.handleMovie = this.handleMovie.bind(this);
-    // this.movieDelete = this.movieDelete.bind(this);
+    this.movieDelete = this.movieDelete.bind(this);
   }
 
   componentDidMount() {
@@ -21,29 +21,26 @@ class MovieDetails extends Component {
   }
 
   async handleMovie() {
-    const { getMovie } = movieAPI;
     const { match } = this.props;
     const { id } = match.params;
     const data = await getMovie(id);
     this.setState({ movies: data, load: false });
   }
 
-  // async movieDelete() {
-  //   const { deleteMovie } = movieAPI;
-  //   const { id } = this.props.match.params;
-  //   const deleted = await deleteMovie(id);
-  //   this.setState({ movies: deleted, load: false })
-  // }
+  async movieDelete() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const deleted = await deleteMovie(id);
+    this.setState({ movies: deleted });
+  }
 
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
     const { movies, load } = this.state;
     const { id, title, storyline, imagePath, genre, rating, subtitle } = movies;
     if (load) {
       return <Loading />;
     }
+
     return (
       <div data-testid="movie-details" className="movie-datails">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
@@ -55,6 +52,9 @@ class MovieDetails extends Component {
         {/* <link to="/">DELETAR</link> */}
         <button type="button"><Link to="/">VOLTAR</Link></button>
         <button type="button"><Link to={ `/movies/${id}/edit` }>EDITAR</Link></button>
+        <button type="button" onClick={ this.movieDelete }>
+          <Link to="/">DELETAR</Link>
+        </button>
       </div>
     );
   }
