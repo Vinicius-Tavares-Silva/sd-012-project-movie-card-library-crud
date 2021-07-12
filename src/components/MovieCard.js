4254ff6e4140e808b9e43850cd98cefbcf373f8d
Janderson Soares <jandinsm@gmail.com>
2021-07-11 23:29:32 -0300

    Desafio 2, loading, -verificar com alguem depois e esta correto

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <h4 className="movie-card-title">{ title }</h4>
+        <img alt={ `Imagem do filme ${title}` } src={ imagePath } />
+        <p className="movie-card-storyline">{ storyline }</p>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+// import movies from '../services/movieData';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,13 +9,28 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+    this.fetchM = this.fetchM.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchM();
+  }
+
+  async fetchM() {
+    const fetchMovies = await movieAPI.getMovies();
+    this.setState({
+      movies: [...fetchMovies],
+      loading: false,
+    });
+    const { movies } = this.state;
+    console.log(movies);
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    const { movies, loading } = this.state;
+    if (loading) return <h1>Carregando...</h1>;
 
     return (
       <div data-testid="movie-list">
