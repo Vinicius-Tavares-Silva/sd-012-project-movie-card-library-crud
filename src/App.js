78dc7b0f9cc7f4dab656651a4f4cc28b2c6229a1
Julio Pereira <jandrade.pereira1@gmail.com>
2021-07-11 22:44:02 -0300

    Requisito 1 - Inclui as exigencias do requisito e comenta paramentros ainda nao utilizados para passar no lint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router, Route } from 'react-router-dom';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <Router>
+        <div>Movie Card Library CRUD</div>
+        <div>
+          <Route path="/" component={ MovieList } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="" component={ NotFound } />
+        </div>
+      </Router>
+    </BrowserRouter>
   );
 }
 
