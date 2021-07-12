69a0c976513640e7a6dc9ce2100f0b94dc85a35d
Álvaro Palma <palma.alvaro@outlook.com>
2021-07-12 17:09:02 -0300

    Cria rotas para cada pagina

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { NotFound } from 'http-errors';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route patch="/" exact component={ MovieList } />
+        <Route patch="/movies/:id" exact component={ MovieDetails } />
+        <Route patch="/movies/new" exact component={ NewMovie } />
+        <Route patch="/movies/:id/edit" exact component={ EditMovie } />
+        <Route patch="" component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
