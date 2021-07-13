48e704b2204da04c23cfd719302dd5b279a8f3ce
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-07-13 10:43:03 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,75 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { getMovie } from '../services/movieAPI';
+import { Loading } from '../components';
 
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+class MovieDetails extends Component {
+  constructor() {
+    super();
 
-// class MovieDetails extends Component {
-//   render() {
-//     // Change the condition to check the state
-//     // if (true) return <Loading />;
+    this.state = {
+      movie: [],
+      loading: true,
+    };
 
-//     const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.fetchAPI = this.fetchAPI.bind(this);
+    this.renderInfo = this.renderInfo.bind(this);
+  }
 
-//     return (
-//       <div data-testid="movie-details">
-//         <img alt="Movie Cover" src={ `../${imagePath}` } />
-//         <p>{ `Subtitle: ${subtitle}` }</p>
-//         <p>{ `Storyline: ${storyline}` }</p>
-//         <p>{ `Genre: ${genre}` }</p>
-//         <p>{ `Rating: ${rating}` }</p>
-//       </div>
-//     );
-//   }
-// }
+  componentDidMount() {
+    this.fetchAPI();
+  }
 
-// export default MovieDetails;
+  async fetchAPI() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    const movieObject = await getMovie(id);
+    this.setState({
+      movie: movieObject,
+      loading: false,
+    });
+  }
+
+  renderInfo() {
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
+
+    return (
+      <div data-testid="movie-details">
+        <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h4>{ `Título: ${title}` }</h4>
+        <p>{ `Subtitle: ${subtitle}` }</p>
+        <p>{ `Storyline: ${storyline}` }</p>
+        <p>{ `Genre: ${genre}` }</p>
+        <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>
+          EDITAR
+        </Link>
+        <Link to="/">
+          VOLTAR
+        </Link>
+      </div>
+    );
+  }
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <div>
+        {loading ? <Loading /> : this.renderInfo()}
+      </div>
+    );
+  }
+}
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
+
+export default MovieDetails;
