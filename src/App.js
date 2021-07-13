c5d367013de39597c6fce58416a2891d6c587be3
Wadson <wadson.dev92@gmail.com>
2021-07-12 21:54:27 -0300

    Requisito 1 Pass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <Route path="/" render={ (props) => <MovieList { ...props } /> } />
+      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+      <Route path="/movies/:new" render={ (props) => <NewMovie { ...props } /> } />
+      <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+      <Route render={ (props) => <NotFound { ...props } /> } />
+    </BrowserRouter>
   );
 }
 
