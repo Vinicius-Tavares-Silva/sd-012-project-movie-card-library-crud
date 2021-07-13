618ff911156b8e6d34f7e1ad828ff7497ae6fc2a
Wadson <wadson.dev92@gmail.com>
2021-07-13 01:16:58 -0300

    Requisito 3 finalizado

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{ title }</h1>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+// Tentei adicionar com o arrayOf e ele buscou o array e passou, mas deu um warning gigante.
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf(Object).isRequired,
+};
 
 export default MovieCard;
