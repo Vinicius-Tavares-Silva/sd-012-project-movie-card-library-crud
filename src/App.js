fccf86cc34c2f31f087be72e017267d0d3ea438b
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-13 17:17:17 -0300

    Concluido requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NotFound from './pages/NotFound';
+import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
 
 function App() {
   return (
-    <div> Iniciando meu projeto </div>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="" component={ NotFound } />
+      </div>
+    </BrowserRouter>
   );
 }
 
