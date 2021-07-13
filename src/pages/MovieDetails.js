d6e125ac6bb051f5f6f6e9b6745c16dfd03dcdc1
André Dantas <aluizcfd@gmail.com>
2021-07-13 07:12:03 -0300

    Implementação do component MovieDetails, correção de rotas em MovieCard e App

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-
-import * as movieAPI from '../services/movieAPI';
-import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import { getMovie } from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      movieCard: {},
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.fetchDetails(id);
+  }
+
+  async fetchDetails(id) {
+    const details = await getMovie(id);
+    this.setState({
+      movieCard: details,
+      isLoading: false,
+    });
+  }
+  
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    const { movieCard, isLoading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movieCard;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (isLoading) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-details">
+        <h4>{title}</h4>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <button type="button">
+          <Link to="/">VOLTAR</Link>
+        </button>
+        <button type="button">
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        </button>
       </div>
     );
   }
