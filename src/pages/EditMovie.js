8ebc059728247a82f2409112511141cda945b0bf
Ana Laura <analauraalves25@gmail.com>
2021-07-13 22:05:06 -0300

    adiciona pagina que edita os filmes

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      movie: {},
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.requestUpdate = this.requestUpdate.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  componentDidMount() {
+    this.requestUpdate();
+  }
+
+  async handleSubmit(updatedMovie) {
+    const { updateMovie } = movieAPI;
+    await updateMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
+  }
+
+  requestUpdate() {
+    const { getMovie } = movieAPI;
+    const { match: { params: { id } } } = this.props;
+    getMovie(id).then((upMovie) => (
+      this.setState({
+        status: '',
+        movie: upMovie,
+      })
+    ));
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
