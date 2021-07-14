4486fb9b8639eccebf6edf1d5e7a39f067237121
Gutemberg <gttj2010@hotmail.com>
2021-07-14 10:43:51 -0300

    Refatoraçao do App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+      <Route path="/" render={ (props) => <MovieList { ...props } /> } />
+      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+      <Route path="/movies/:new" render={ (props) => <NewMovie { ...props } /> } />
+      <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+      <Route render={ (props) => <NotFound { ...props } /> } />
+    </BrowserRouter>
     </Router>
   );
 }
