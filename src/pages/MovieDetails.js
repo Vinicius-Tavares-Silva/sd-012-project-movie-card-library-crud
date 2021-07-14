0ab3386ac2cc3f150c94f008a78d81bd800529b7
Ana Laura <analauraalves25@gmail.com>
2021-07-13 21:19:45 -0300

    adiciona paginas de detalhes dos filmes

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,56 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      details: undefined,
+    };
+
+    this.requestDetails = this.requestDetails.bind(this);
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    return this.requestDetails(id);
+  }
+
+  async requestDetails(id) {
+    const { getMovie } = movieAPI;
+    const request = await getMovie(id)
+      .then((movie) => (
+        this.setState({
+          details: movie,
+        })
+      ));
+    return request;
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { details } = this.state;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (!details) return <Loading />;
 
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = details;
     return (
       <div data-testid="movie-details">
+        <button type="button">
+          <Link to="/">VOLTAR</Link>
+        </button>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h1>{ `Title: ${title}` }</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <button type="button">
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        </button>
       </div>
     );
   }
