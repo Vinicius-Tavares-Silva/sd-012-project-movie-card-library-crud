32b1cdba9ad29e49abb6d0db611ac2a9fa90f935
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-07-13 23:32:15 -0300

    Termina requesito 2/renderizar a pagina com loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <p>Movie Card Library CRUD</p>
-    </Router>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <div>
+          <p>Movie Card Library CRUD</p>
+        </div>
+        <div>
+          <BrowserRouter>
+            <Route exact path="/" component={ MovieList } />
+            <Route exact path="/movies/:id" component={ MovieDetails } />
+            <Route exact path="/movies/new" component={ NewMovie } />
+            <Route exact path="/movies/:id/edit" component={ EditMovie } />
+            <Route exact path="*" component={ NotFound } />
+          </BrowserRouter>
+        </div>
+      </main>
+    );
+  }
 }
-
 export default App;
