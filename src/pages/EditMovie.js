22cf0636925c565661b2c2651c59d563d9489951
Victor <juanvictor70@gmail.com>
2021-07-14 02:14:37 -0400

    implementando a adicao de filme

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      movie: [],
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  /* handleSubmit(updatedMovie) {
-  } */
+  componentDidMount() {
+    const { match: { param: { id } } } = this.props;
+    const { getMovie } = movieAPI;
+
+    getMovie(id);
+    this.saveMovie();
+  }
+
+  handleSubmit(updatedMovie) {
+    const { createMovie } = movieAPI;
+    createMovie(updatedMovie);
+    this.setState({
+      movie: updateMovie,
+      shouldRedirect: true,
+    });
+  }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
