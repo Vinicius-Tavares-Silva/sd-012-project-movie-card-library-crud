fe5e1cadce7fec57ea507faab6cb6e4a279de2c0
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-07-14 13:27:22 -0300

    Requisito 6

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
+import { Redirect } from 'react-router-dom';
 
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
@@ -6,18 +8,39 @@ import * as movieAPI from '../services/movieAPI';
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isLoaded: false,
+      shouldRedirect: false
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    this.setState({ isLoaded: true });
+    movieAPI.createMovie(newMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
   }
 
   render() {
+    const { isLoaded, shouldRedirect } = this.state;
+    if (shouldRedirect) {
+      return <Redirect to="/" />
+    }
+
+    if (isLoaded) {
+      return <Loading />
+    }
+
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={ this.handleSubmit } />
+        <MovieForm onSubmit={this.handleSubmit} />
       </div>
     );
   }
 }
+
 export default NewMovie;
