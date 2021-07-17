4cb3bd382fe9df95642cc259dc25a31555d42a9e
Martin <escorpmartin97@gmail.com>
2021-07-17 18:43:06 -0300

    [req 1] [acria rotas da app]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>Movie Card Library CRUD</div>
-      </Router>
+      <div>
+        <BrowserRouter>
+          <h1>Movie Card Library CRUD</h1>
+          <Switch>
+            <Route exact path="/" component={ MovieList } />
+
+            <Route exact path="/movies/new" component={ NewMovie } />
+
+            <Route exact path="/movies/:id" component={ MovieDetails } />
+
+            <Route exact path="/movies/:id/edit" component={ EditMovie } />
+
+            <Route exact path="" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
