f16f46a25c1f04785f66a030f99c656adf063c9e
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-16 21:47:06 -0300

    Refatora componente MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, id } = movie;
+    const { title, storyline, id, imagePath } = movie;
     return (
       <div data-testid="movie-card">
+        <img alt="Movie Cover" src={ `../${imagePath}` } />
         <h2>{ title }</h2>
         <p>{ storyline }</p>
         <Link to={ `movies/${id}` }>VER DETALHES</Link>
@@ -20,6 +21,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     storyline: PropTypes.string,
     id: PropTypes.number,
+    imagePath: PropTypes.string,
   }).isRequired,
 };
 
