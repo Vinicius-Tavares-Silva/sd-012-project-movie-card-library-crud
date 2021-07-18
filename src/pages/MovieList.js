4334ac8774aa8e9d26885f9e2b1851abcdb669a5
Martin <escorpmartin97@gmail.com>
2021-07-18 13:39:46 -0300

    [req 2 - exibe texto carregando e renderiza ilmes da API]

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import { Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       movies: [],
+      isFetching: true,
     };
+    this.setMovies = this.setMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.setMovies();
+  }
+
+  async setMovies() {
+    const { getMovies } = movieAPI;
+    const response = await getMovies();
+    this.setState({
+      movies: response,
+      isFetching: false,
+    });
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, isFetching } = this.state;
+    if (isFetching) return <Loading />;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
       </div>
     );
   }
