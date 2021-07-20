e0cd09523e30d0f688c417cea2fecf23838a5933
Ed Santos <edsantos2822@gmail.com>
2021-07-20 12:14:40 -0300

    Finalizando - Requisito 3 - Renderizando MovieCard - Corrigindo erros em App, EditMovie e NewMovie

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, subtitle, imagePath, storyline } = movie;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt="Cartaz do filme" />
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>{storyline}</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf({}).isRequired,
+};
 
 export default MovieCard;
