be0bc52fa8f5f2cb8b557d53ac903b8812b72e9a
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-24 00:02:23 -0300

    Concluido requisito 6

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-
-import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
+import { MovieForm } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      shouldRedirect: false,
+    };
   }
 
-  handleSubmit() {
-    // newMovie
+  async handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie).finally(() => this.setState({ shouldRedirect: true }));
   }
 
   render() {
+    const { shouldRedirect } = this.state;
+
+    if (shouldRedirect) {
+      return <Redirect to="/" />;
+    }
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
