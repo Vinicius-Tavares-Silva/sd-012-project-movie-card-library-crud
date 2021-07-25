8d630a47e39ff70295704cac28a3eb5a6af07762
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-07-25 02:14:59 -0300

    Resoluçao do Req 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,17 +9,28 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  async componentDidMount() {
+    const movies = await movieAPI.getMovies();
+    this.setState({
+      movies: movies,
+    }, () => this.setState({
+      loading: false,
+    }));
+  }
 
+  render() {
+    const { movies, loading } = this.state;
+    console.log(movies);
+    const wait = (loading ? (<h1>Carregando...</h1>) : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />));
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {wait}
       </div>
     );
   }
