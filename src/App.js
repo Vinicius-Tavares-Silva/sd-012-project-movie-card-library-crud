c2875bbccdb8ba5c856efe0ed2dede27f7421570
Alice Gonçalves <galice9992@gmail.com>
2021-07-26 15:37:39 -0300

    resolve requisito 1, criar rotas para as paginas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-// import React from 'react';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+
+      <Route path="/">
+        <MovieList />
+      </Route>
+
+      <Route path="/movies/:id">
+        <MovieDetails />
+      </Route>
+
+      <Route path="/movies/new">
+        <NewMovie />
+      </Route>
+
+      <Route path="/movies/:id/edit">
+        <EditMovie />
+      </Route>
+
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </BrowserRouter>
   );
 }
 
