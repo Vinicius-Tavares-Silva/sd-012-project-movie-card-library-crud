b5c42a768c5fa3e857689d3611279073b30fb1f1
Alice Gonçalves <galice9992@gmail.com>
2021-07-26 16:31:48 -0300

    resolve requisito 3, coloca elementos do filme dentro de MovieCard e faz um link para MovieDetails

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ movie.imagePath } alt="Banner" />
+        <h3>{movie.title}</h3>
+        <h4>{movie.subtitle}</h4>
+        <p>{movie.storyline}</p>
+        <Link to={ `/movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
