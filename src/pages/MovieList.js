febb6d9c93f8054ae341d3cd7cd7454ca1a504de
Ed Santos <edsantos2822@gmail.com>
2021-07-25 22:31:01 -0300

    Finalizando - Requisito 6 - Adiciona funcionalidade de adicionar novos cards

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
@@ -32,10 +33,9 @@ class MovieList extends Component {
   render() {
     const { movies, loading } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
         {loading
           ? <Loading />
           : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
