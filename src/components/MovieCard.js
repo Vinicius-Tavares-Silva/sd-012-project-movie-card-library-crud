e758ccb0412e864d106909c956c9a7c55d3102e1
Lucas Basques <lukasbasques@gmail.com>
2021-07-25 23:23:17 -0300

    Requisito 2

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movies } = this.props;
+
     return (
       <div data-testid="movie-card">
         Movie Card
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  movies: PropTypes.shape(PropTypes.object).isRequired,
 }
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,19 +10,34 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    const fetchResult = await movieAPI.getMovies();
+    this.setState({
+      loading: false,
+      movies: fetchResult,
+    });
   }
 
   render() {
     const { movies } = this.state;
+    const { loading } = this.state;
 
-    // Render Loading here if the request is still happening
-
+    if (loading === true) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
-    );
+      </div>);
   }
 }
 
