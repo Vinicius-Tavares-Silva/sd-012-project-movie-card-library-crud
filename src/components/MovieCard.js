132fbaed6b086ca90c0b740cac1d3dbcfe5f10d7
Kevin Willian <kevinwillian084@gmail.com>
2021-07-25 23:50:39 -0300

    Add requisite 03

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, storyline, imagePath } } = this.props;
+    const { movie: { title, storyline, imagePath, id } } = this.props;
     return (
       <div data-testid="movie-card">
         <img src={ imagePath } alt={ title } />
         <p>{ title }</p>
         <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
