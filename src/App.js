681cc0d070e466e31328f31df4698abe7b348788
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-07-26 11:33:10 -0300

    Implementando as rotas das páginas no App (sem corrigir o lint)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-// Iniciando o projeto #VQV
-
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <header>
+      <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+        <Route path="/" render={ () => <MovieList /> } />
+        <Route path="/movies/:id" render={ () => <MovieDetails /> } />
+        <Route path="/movies/new" render={ () => <NewMovie /> } />
+        <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
+        <Route render={ () => <NotFound /> } />
+      </BrowserRouter>
+    </header>
   );
 }
 
