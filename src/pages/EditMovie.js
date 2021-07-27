6b342e472419b29570b74d5b41edb10dcd945e94
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-27 12:24:43 -0400

    Requisito 5 - EditMovies exibe tela carregando durante requisição

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { MovieForm, Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: {},
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    movieAPI.getMovie(id)
+      .then((result) => {
+        console.log(result);
+        this.setState({
+          status: 'success',
+        });
+      });
+  }
+
+  handleSubmit(updatedMovie) {
+    console.log(updatedMovie);
+  }
+
   render() {
+    const { status, shouldRedirect, movie } = this.state;
+    if (shouldRedirect) {
+      // Redirect
+    }
+
+    if (status === 'loading') {
+      // render Loading
+      return <Loading />;
+    }
+
     return (
       <div data-testid="edit-movie">
-        <p>Estou no EditMovie</p>
+        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
 }
 
 export default EditMovie;
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
