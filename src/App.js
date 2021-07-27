349e5ad3f0318e30bdc6e2d2079e92e01cb95a19
Djaniza Vasques <djanizav@gmail.com>
2021-07-27 03:52:56 -0300

    Renderiza BrowserRouter e rotas para as paginas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Router exact path="/" Component={ MovieList } />
+          <Router exact path="/movies/:id" Component={ MovieDetails } />
+          <Router exact path="/movies/new" Component={ NewMovie } />
+          <Router exact path="/movies/:id/edit" Component={ EditMovie } />
+          <Router Component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
