cbe545977a452078cdfed5b40ea69f97658b2e91
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-27 10:44:42 -0300

    implementa fetch ao filme desejado em details

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-
-import * as movieAPI from '../services/movieAPI';
+import PropTypes from 'prop-types';
+import { getMovie } from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      movie: '',
+    };
+
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie() {
+    const { match } = this.props;
+    const { id } = match.params;
+    getMovie(id)
+      .then((resolve) => this.setState({
+        loading: false,
+        movie: resolve,
+      }));
+  }
+
   render() {
     // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { loading, movie } = this.state;
+    if (loading) return <Loading />;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `tile: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
@@ -21,5 +47,13 @@ class MovieDetails extends Component {
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
