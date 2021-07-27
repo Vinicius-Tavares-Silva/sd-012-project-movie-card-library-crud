a7f7ee412f883e3fd77e2e289bd88dc33e64c5ae
LucasPeixoto <peixotolucas516@gmail.com>
2021-07-27 02:13:22 -0300

    editMovie mounted

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
 
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      movie: {},
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    const { getMovie } = movieAPI;
+    const { match } = this.props;
+    const { id } = match.params;
+    getMovie(id).then((movie) => this.setState(
+      {
+        status: 'notLoading',
+        movie,
+      },
+    ));
+  }
+
   handleSubmit(updatedMovie) {
+    const { updateMovie } = movieAPI;
+    updateMovie(updatedMovie).then(() => {
+      this.setState({
+        shouldRedirect: true,
+      });
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
       // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
       // render Loading
+      return <Loading />;
     }
 
     return (
@@ -30,5 +56,10 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.object,
+  id: PropTypes.string,
+}.isRequired;
 
 export default EditMovie;
