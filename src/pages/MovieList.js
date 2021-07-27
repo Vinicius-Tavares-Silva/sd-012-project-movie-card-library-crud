4f9feda095cf42e503d9522cf69a5e45fd4a3bb6
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-27 10:20:00 -0400

    Requisito 2 - Renderiza o 'Carregando...' no MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -8,18 +9,22 @@ class MovieList extends Component {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
   }
 
   componentDidMount() {
-    movieAPI.getMovies().then((result) => this.setState({ movies: result }));
+    movieAPI.getMovies()
+      .then((result) => this
+        .setState({ movies: result, loading: false }));
   }
 
   render() {
-    const { movies } = this.state;
+    const { loading } = this.state;
+    if (loading) return <Loading />;
 
-    // Render Loading here if the request is still happening
+    const { movies } = this.state;
 
     return (
       <div data-testid="movie-list">
