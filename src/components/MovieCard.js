f1e9d949cd87a4dd3a52b34f6822aa6e096735f8
Djaniza Vasques <djanizav@gmail.com>
2021-07-27 03:58:39 -0300

    Completa o codigo de MovieCard e insere link para pagina de detalhes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, imagePath, id } = movie;
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div className="movie-card" data-testid="movie-card">
+        <img alt="Movie Cover" src={ imagePath } />
+        <h4>{title}</h4>
+        <p>{storyline}</p>
+        <div>
+          <Link to={ `/movies/${id}` }> VER DETALHES</Link>
+        </div>
       </div>
     );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]),
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  }).isRequired,
+};
