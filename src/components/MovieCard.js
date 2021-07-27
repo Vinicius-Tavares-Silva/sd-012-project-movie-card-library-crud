0fcacb69c636487895ecadeb1c9e6fd1c26d782b
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-27 10:41:34 -0400

    Requisito 3 - Renderiza o link para ver detalhes dos cards

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 // import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, id } = movie;
     return (
       <div className="movie-card" data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
@@ -13,6 +14,7 @@ class MovieCard extends React.Component {
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
+          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
         </div>
         {/* <Rating rating={ rating } /> */}
       </div>
@@ -27,6 +29,7 @@ MovieCard.propTypes = {
     storyline: PropTypes.string,
     rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     imagePath: PropTypes.string,
+    id: PropTypes.number,
   }).isRequired,
 };
 
