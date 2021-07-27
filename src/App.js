dbfe0b0af3adab8be54e042715fbed81b8b41500
Uriel <urielbochi@gmail.com>
2021-07-27 13:49:02 -0300

    Corrigindo requisito 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import MovieList from './pages/MovieList';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" />
-        <Route path="/movies/new" />
-        <Route path="/movies/:id/edit" />
-        <Route path="/movies/:id" />
-        <Route path="*" />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="*"  component={ NotFound } />
       </Switch>
     </Router>
   );
