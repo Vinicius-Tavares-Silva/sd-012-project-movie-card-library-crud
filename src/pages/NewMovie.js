b9c4195f0b9df1982f9742f987455c4b8896bce4
Uriel <urielbochi@gmail.com>
2021-07-28 00:46:28 -0300

    Enviando requisitos 6 + 7, falta corrigir todos os erros do LINT + PROPS VALIDATION

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -6,13 +6,30 @@ import * as movieAPI from '../services/movieAPI';
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      shouldRedirect: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(newMovie) {
+  async handleSubmit(newMovie) {
+    const createMovie = await movieAPI.createMovie(newMovie);
+    if (createMovie === 'OK') {
+      this.setState({
+        shouldRedirect: true,
+      });
+    }
   }
 
   render() {
+    const { shouldRedirect } = this.state;
+
+    if (shouldRedirect) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
@@ -20,4 +37,5 @@ class NewMovie extends Component {
     );
   }
 }
+
 export default NewMovie;
