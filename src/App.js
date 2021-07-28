60650533966f0898842e0eb3cc956b190678ab54
Lucas Santos <lucassantos_dev@outlook.com>
2021-07-28 08:42:29 -0300

    dois dos cinco casos do req 1, done...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ <MovieList /> } />
+        {// aqui vai route params}
+        }
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route path="/movies/new" component={ <NewMovie /> } />
+        {// aqui tbm vai route params}
+        }
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+      </BrowserRouter>
+
+    );
+  }
 }
 
 export default App;
