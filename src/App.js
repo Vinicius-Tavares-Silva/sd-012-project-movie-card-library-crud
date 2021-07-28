3587a08657d99bd98f25cdfa10f02edc52584764
Lucas Santos <lucassantos_dev@outlook.com>
2021-07-28 10:04:23 -0300

    ue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ MovieList } />
-        {// aqui vai route params}
-        }
-        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-        <Route path="/movies/new" component={ NewMovie } />
-        {// aqui tbm vai route params}
-        }
-        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+        <h1>Movie Card Library CRUD</h1>
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route
+            path="/movies/:id/edit"
+            render={ (props) => <EditMovie { ...props } /> }
+          />
+          <Route
+            path="/movies/:id"
+            render={ (props) => <MovieDetails { ...props } /> }
+          />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/notfound" component={ NotFound } />
+          <Redirect to="/notfound" />
+        </Switch>
       </BrowserRouter>
 
     );
