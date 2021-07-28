3cdfce8520ea2a8e3aa93453abd68b2901655628
Uriel <urielbochi@gmail.com>
2021-07-28 00:07:00 -0300

    Requisito 4? Provavel com erros de lint (falta validar props)

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,60 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      loading: true,
+      movie: {},
+    };
+
+    this.fetchAPI = this.fetchAPI.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  fetchAPI() {
+    const { match: { params: { id } } } = this.props;
+    this.setState(
+      { loading: true },
+      async () => {
+        const getMovie = await movieAPI.getMovie(id);
+        this.setState({
+          loading: false,
+          movie: getMovie,
+        });
+      },
+    );
+  }
+
+  render() {
+   
+    const { movie:
+      { title, storyline, imagePath, genre, rating, subtitle, id },
+    } = this.state;
+    const { loading } = this.state;
+
+    if (loading) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h1>{ title }</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
       </div>
     );
   }
