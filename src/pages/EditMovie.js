edc63305c4c03544b21097fd880c757c2c25ff3e
Aldria Melo <aldriamelo.academico@gmail.com>
2021-07-27 22:47:45 -0300

    adiciona requisito 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
+import { Loading, MovieForm } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      carregando: true,
+      redireciona: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.editaFilme = this.editaFilme.bind(this);
   }
 
-  // handleSubmit(updatedMovie) {
-  // }
+  componentDidMount() {
+    this.editaFilme();
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState({
+      redireciona: true,
+    });
+  }
+
+  async editaFilme() {
+    const { match: { params: { id } } } = this.props;
+    const edicao = await movieAPI.getMovie(id);
+    this.setState({
+      movie: edicao,
+      carregando: false,
+    });
+  }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
+    const { status, redireciona, movie, carregando } = this.state;
+    const { match: { params: { id } } } = this.props;
+    if (redireciona) {
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
@@ -25,7 +48,8 @@ class EditMovie extends Component {
 
     return (
       <div data-testid="edit-movie">
-        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
+        {carregando ? <Loading />
+          : <MovieForm movie={ movie } onSubmit={ this.handleSubmit } id={ id } />}
       </div>
     );
   }
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
+    this.alteraMovies = this.alteraMovies.bind(this);
+    this.state = {
+      movies: {},
+      carregando: true,
+    };
+  }
 
-    // const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  componentDidMount() {
+    this.alteraMovies();
+  }
+
+  async alteraMovies() {
+    const { match: { params: { id } } } = this.props;
+    const requisicao = await movieAPI.getMovie(id);
+    this.setState({
+      movies: requisicao,
+      carregando: false,
+    });
+  }
+
+  render() {
+    const { movies, carregando } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movies;
+    const { match: { params: { id } } } = this.props;
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+      <div>
+        {carregando ? <Loading /> : (
+          <div data-testid="movie-details">
+            <img alt="Movie Cover" src={ `../${imagePath}` } />
+            <p>{ `Title: ${title}` }</p>
+            <p>{ `Subtitle: ${subtitle}` }</p>
+            <p>{ `Storyline: ${storyline}` }</p>
+            <p>{ `Genre: ${genre}` }</p>
+            <p>{ `Rating: ${rating}` }</p>
+            <button type="button">
+              <Link to="/">VOLTAR</Link>
+            </button>
+            <button type="button">
+              <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+            </button>
+          </div>
+        )}
       </div>
     );
   }
