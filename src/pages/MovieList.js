764e0331289872911c4e83ded69b6e58152b6b14
Aldria Melo <aldriamelo.academico@gmail.com>
2021-07-27 23:24:39 -0300

    adiciona requisito 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -33,6 +33,7 @@ class MovieList extends Component {
 
     return (
       <div data-testid="movie-list">
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
         {carregando ? <Loading /> : movies.map((movie) => (
           <MovieCard key={ movie.title } movie={ movie } />))}
       </div>
